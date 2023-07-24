3e80bea3e3586bb49c8f6e6b0b9a3039da53052d
VincnetNgo12 <duchuy30092005@gmail.com>
2023-07-24 14:36:35 -0600

    Added Image Upload

--- app/src/main/java/com/example/vincballs/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/vincballs/ui/gallery/GalleryFragment.java
@@ -1,37 +1,75 @@
 package com.example.vincballs.ui.gallery;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.vincballs.R;
 import com.example.vincballs.databinding.FragmentGalleryBinding;
 
+import java.io.IOException;
+
 public class GalleryFragment extends Fragment {
+    private static final int SELECT_IMAGE = 1;
+    ImageView ivUpload;
+    Button btnUpload;
 
-    private FragmentGalleryBinding binding;
-
+    @SuppressLint("MissingInflatedId")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        ivUpload = root.findViewById(R.id.ivUpload);
+        btnUpload = root.findViewById(R.id.btnUpload);
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, SELECT_IMAGE);
+            }
+        });
         return root;
     }
 
+
     @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_IMAGE && resultCode == Activity.RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            if (selectedImageUri != null) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImageUri);
+                    ivUpload.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
